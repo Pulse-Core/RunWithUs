c5b947ce5ced10acaba792e650dddd80548d34c5
Sardor Madaminov <sardor0968@gmail.com>
2024-04-11 16:24:00 +0900

    merged into main branch

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
 
-function AppRoutes() {
-  return <Router></Router>;
+function Routes() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </div>
+    </Router>
+  );
 }
 
-export default AppRoutes;
+export default Routes;
